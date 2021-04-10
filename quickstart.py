4429b34427bd856325b5f50d736c336e4464b7c3
Joseph Lee <jslee08@email.wm.edu>
2021-04-10 15:17:39 -0400

    can now display an email based on a search query

--- quickstart.py
+++ quickstart.py
@@ -14,19 +14,106 @@
 
 # [START gmail_quickstart]
 from __future__ import print_function
+import sys
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
+import email
+import base64
+import random
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
-def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
+
+def get_message(service, user_id, msg_id):
+    try:
+        message = service.users().messages().get(
+            userId=user_id, id=msg_id, format='raw').execute()  # "raw" means byte format-based, and we need to decode
+
+        msg_raw = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
+
+        # print(msg_raw)
+
+        msg_str = email.message_from_bytes(msg_raw)
+
+        # print(msg_str)
+
+        content_type = msg_str.get_content_maintype()
+
+        if content_type == "multipart":
+            main_msg = msg_str.get_payload()[0]
+
+            print("this is the main message object:")
+            print(main_msg)
+
+            # part 1 is plain text, part 2 is html text
+            print("get payload of this main message")
+            print(main_msg.get_payload())
+
+            part1, part2 = main_msg.get_payload()
+
+            print("This is the message body: ")
+            print(part1.get_payload())
+            return part1.get_payload()
+
+        else:
+            return msg_str.get_payload()
+
+    except ValueError as vE:
+        print(f"An error occurred: {vE}")
+        print(
+            "Safely ignore this error if you see 'too many values to unpack (expected 2)'")
+
+
+def search_messages(service, user_id, search_string):
+    try:
+        # retrieve the email message ids based on a search query
+
+        # If there are no search results, it looks like this: {'resultSizeEstimate': 0}
+
+        search_ids = service.users().messages().list(
+            userId=user_id, q=search_string).execute()
+
+        # print(search_ids)
+
+        # handle the case that there are no search results
+        number_results = search_ids['resultSizeEstimate']
+
+        final_list = []
+
+        if number_results > 0:
+            message_id_list = search_ids['messages']
+
+            # message_ids has this format:
+            '''
+            {'messages': [{'id': ..., 'threadId': ...}, 
+            ...,
+            {'id': ..., 'threadId': ...}],
+            'resultSizeEstimate': #}
+            '''
+
+            for ids in message_id_list:
+                # ids has format {'id': ..., 'threadId'...}
+                final_list.append(ids['id'])
+
+            # print(final_list)
+
+            return final_list
+
+        else:  # let the user know there were no search results and return an empty string
+            print("There were no results for your search. ")
+            print("Returning an empty string...")
+            return ""
+
+    except:
+        print(f"An error occurred: {sys.exc_info()[0]}")
+
+
+def get_service():
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -47,17 +134,21 @@ def main():
 
     service = build('gmail', 'v1', credentials=creds)
 
-    # Call the Gmail API
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
+    return service
 
-    if not labels:
-        print('No labels found.')
-    else:
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
 
 if __name__ == '__main__':
-    main()
+    # main()
+    service = get_service()
+    # print(service)
+
+    search_query = input("Tell me something to search for: ")
+    message_ids = search_messages(
+        service=service, user_id='me', search_string=search_query)
+
+    if message_ids:
+        print(get_message(service=service,
+              user_id='me', msg_id=random.choice(message_ids)))
+
+
 # [END gmail_quickstart]
